4d243f86da2db3017be0617e8ef869baeb44d925
Dalton Spillman <dalton_spillman@yahoo.com>
2018-12-09 19:49:07 -0600

    Created Equals and DoesNotEqual

---
+++ Solution/SmoothValidator/Extensions/SmoothExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SmoothValidator.Extensions
+{
+    public static partial class SmoothExtensions
+    {
+        // Equals
+        public static SmoothValidatorBuilder<TObject, TError> Equals<TObject, TError>(
+            this SmoothValidatorBuilder<TObject, TError> builder,
+            Func<TObject> objectProvider,
+            Func<TError> errorProvider
+        )
+        {
+            return builder.True(
+                value => EqualityComparer<TObject>.Default.Equals(value, objectProvider.Invoke()), 
+                errorProvider
+            );
+        }
+
+        // DoesNotEqual
+        public static SmoothValidatorBuilder<TObject, TError> DoesNotEqual<TObject, TError>(
+            this SmoothValidatorBuilder<TObject, TError> builder,
+            Func<TObject> objectProvider,
+            Func<TError> errorProvider
+        )
+        {
+            return builder.True(
+                value => !EqualityComparer<TObject>.Default.Equals(value, objectProvider.Invoke()), 
+                errorProvider
+            );
+        }
+    }
+}
